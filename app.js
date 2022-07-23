4ceb03b69450686507cde5c32eb6617ca4eda351
[ferdinandmulimi] <[ferdinand.mulimi@student.moringaschool.com]>
2022-07-23 19:58:31 +0300

    modified app.js

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-// Import express framework
+
+// Import express package
 const express = require('express');
 
 // Initialize express
 const app = express();
 
+// route for the index page
+app.get('/', (req, res)=>{
+   response.send('<h1> Welcome to my app</h1>')
+});
+
 // Define the port number
-const PORT = 5000;
+const PORT = 8000;
 
-
-app.listen(PORT, function(){
+app.listen(PORT, ()=>{
    console.log(`Server is listening on port ${PORT}`)
 })
