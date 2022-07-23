352a177e01ef1cd5edea952ae47e7a297a197ec9
[ferdinandmulimi] <[ferdinand.mulimi@student.moringaschool.com]>
2022-07-23 20:21:14 +0300

    updated css file

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-
 // Import express package
 const express = require('express');
+
 
 // Initialize express
 const app = express();
 
-// route for the index page
-app.get('/', (req, res)=>{
-   response.send('<h1> Welcome to my app</h1>')
-});
+// Set a static folder
+app.use(express.static('public'));
 
 // Define the port number
 const PORT = 8000;
